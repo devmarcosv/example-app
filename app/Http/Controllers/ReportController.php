ec91b95fab7bedf28054e268609b84fd9c67a14f
devmarcosv <devmarcosvinicius@gmail.com>
2023-12-05 10:56:33 -0500

    fix reports

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,21 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Colaborador;
-use App\Models\Unidade;
+use App\Models\Collaborators;
+use App\Models\Unit;
 use Illuminate\Http\Request;
 
 class ReportController extends Controller
 {
     public function colaboradores()
     {
-        $relatorio = Colaborador::with(['cargo', 'unidade'])->select('nome', 'cpf', 'email', 'unidade_id', 'cargo_id')->get();
+        $relatorio = Colaborador::with(['unidade'])->select('nome', 'cpf', 'email', 'unidade_id')->get();
         return response()->json($relatorio);
     }
 
     public function totalColaboradoresPorUnidade()
     {
-        $relatorio = Unidade::withCount('colaboradores')->select('nome_fantasia', 
+        $relatorio = Unit::withCount('colaboradores')->select('nome_fantasia', 
         'razao_social', 
         'cnpj', 
         'colaboradores_count as total_colaboradores')
@@ -27,7 +27,7 @@ class ReportController extends Controller
 
     public function rankingColaboradores()
     {
-        $relatorio = Colaborador::with(['cargo', 'unidade'])
+        $relatorio = Colaboradors::with(['unidade'])
             ->orderByDesc('nota_desempenho')
             ->select('nome', 'cpf', 'email', 'unidade_id', 'nota_desempenho')
             ->get();
